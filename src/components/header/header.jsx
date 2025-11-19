837b8cdd67a12737c3988a1b7f7f7e1897d47838
laurapigosso <laurapigosso@gmail.com>
2025-11-19 14:26:54 -0300

    Ajuste header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,39 +1,55 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../../styles/header.css';
 
 import logo_icone from '../../../public/Logo-icone.png'
-
 import Botaoprimario from '../botao_primario/botaoprimario';
 
 function Header() {
   return (
-    <header className="header container-fluid py-3 shadow-sm bg-white">
-      <nav className="navbar navbar-expand-lg container">
+    <header className="header container-fluid shadow-sm">
+      <nav className="navbar navbar-expand-lg container d-flex align-items-center justify-content-between">
 
+        <div className="d-flex d-lg-none align-items-center w-100 position-relative">
+
+          {/* Botão menu (esquerda) */}
+          <button
+            className="navbar-toggler position-absolute start-0"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#menu"
+            aria-controls="menu"
+            aria-expanded="false"
+            aria-label="Menu"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Logo no mobile */}
+          <a className="navbar-brand mx-auto" href="#inicio">
+            <img
+              src={logo_icone}
+              alt="Logotipo da Próxima Jornada"
+              className="img-fluid"
+              style={{ height: "50px" }}
+            />
+          </a>
+        </div>
+
+        {/* DESKTOP: LOGO */}
         <a className="navbar-brand d-none d-lg-block" href="#inicio">
           <img
-            src={logo_icone}  
-            alt="Logotipo da Proxíma Jornada. A logo é um icone de um livro aberto com setas saindo do meio do livro"
+            src={logo_icone}
+            alt="Logotipo da Próxima Jornada"
             className="img-fluid"
-            style={{ height: "50px" }} 
+            style={{ height: "50px" }}
           />
         </a>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#menu"
-          aria-controls="menu"
-          aria-expanded="false"
-          aria-label="Menu"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-end" id="menu">
+        {/* NAVIGATION */}
+        <div className="collapse navbar-collapse justify-content-center" id="menu">
 
-          <ul className="navbar-nav mx-auto gap-3 align-items-lg-center mt-3 mt-lg-0">
+          <ul className="navbar-nav gap-3 align-items-lg-center text-start">
 
             <li className="nav-item">
               <a className="nav-link link" href="#inicio">Início</a>
@@ -50,10 +66,16 @@ function Header() {
             <li className="nav-item">
               <a className="nav-link link" href="#Sobre">Sobre nossa solução</a>
             </li>
+
+            {/* BOTÃO NO MENU (MOBILE) */}
+            <li className="nav-item d-lg-none mt-3">
+              <Botaoprimario text="Faça o teste" />
+            </li>
           </ul>
         </div>
-        
-        <div className="ms-lg-4 mt-3 mt-lg-0">
+
+        {/* BOTÃO NO DESKTOP */}
+        <div className="d-none d-lg-block ms-lg-auto">
           <Botaoprimario text="Faça o teste" />
         </div>
 
